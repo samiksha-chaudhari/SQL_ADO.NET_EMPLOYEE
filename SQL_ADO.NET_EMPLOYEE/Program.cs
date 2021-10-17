11ab1d767ddf9e70c3c274d2a8d5c5c59318a4dc
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-17 18:25:02 +0530

    welcome

---
+++ SQL_ADO.NET_EMPLOYEE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQL_ADO.NET_EMPLOYEE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wELCOME TO ADO .NET");
+        }
+    }
+}
